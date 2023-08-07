b643c9c30ba9fbc5de59f52eb1fa1710a97bf9ce
Mubarak Mayyeri <mubarakmayyeri@gmail.com>
2023-08-07 12:27:27 +0530

    accessing dbs and collections

--- mongodb.py
+++ mongodb.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv, find_dotenv
 from pymongo import MongoClient
+import pprint
 import os
 
 load_dotenv(find_dotenv())
@@ -11,8 +12,15 @@ uri = f"mongodb+srv://mubarakmayyeri:{password}@cluster0.3hujcn7.mongodb.net/?re
 client = MongoClient(uri)
 
 # Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+# try:
+#     client.admin.command('ping')
+#     print("Pinged your deployment. You successfully connected to MongoDB!")
+# except Exception as e:
+#     print(e)
+
+
+dbs = client.list_database_names()
+print("Databases: ", dbs)
+test_db = client.test_db
+collections = test_db.list_collection_names()
+print("Collections in test_db:", collections)
