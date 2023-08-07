7839a0ba2245752c1b0576b61522c91096602119
Mubarak Mayyeri <mubarakmayyeri@gmail.com>
2023-08-07 16:57:40 +0530

    basic querying methods

--- mongodb.py
+++ mongodb.py
@@ -40,21 +40,78 @@ def insert_document(document):
 # insert_document(doc)
 
 db = client.school
-collection = db.students
+studnet_collection = db.students
 
-first_names = ["Alice", "Bob", "Charlie", "Diana", "Ella"]
-last_names = ["Smith", "Johnson", "Williams", "Brown", "Jones"]
-ages = [18, 23, 20, 19, 22]
+# first_names = ["Alice", "Bob", "Charlie", "Diana", "Ella"]
+# last_names = ["Smith", "Johnson", "Williams", "Brown", "Jones"]
+# ages = [18, 23, 20, 19, 22]
 
-docs = []
+# docs = []
 
-for f_name, l_name, age in zip(first_names, last_names, ages):
-    doc = {"first_name": f_name, "last_name":l_name, "age": age}
-    docs.append(doc)
+# for f_name, l_name, age in zip(first_names, last_names, ages):
+#     doc = {"first_name": f_name, "last_name":l_name, "age": age}
+#     docs.append(doc)
 
 
 def insert_document(documents, collection):
     collection.insert_many(documents)
     print("Documents inserted succesfully")
 
-insert_document(docs, collection)
+# insert_document(docs, collection)
+
+printer = pprint.PrettyPrinter()
+def get_documents(collection):
+    cursor = collection.find()
+
+    for i in cursor:
+        printer.pprint(i)
+
+# get_documents(collection)
+
+def get_diana(collection):
+    diana = collection.find_one({"first_name": "Diana"})
+    printer.pprint(diana)
+
+# get_diana(studnet_collection)
+
+def find_count():
+    count = studnet_collection.count_documents(filter={})
+    return count
+
+# print("Number of students: ", find_count())
+
+
+def student_by_id(student_id):
+    from bson.objectid import ObjectId
+
+    _id = ObjectId(student_id)
+
+    student = studnet_collection.find_one({"_id": _id})
+    printer.pprint(student)
+
+
+# student_by_id("64d09c52b85fcd3cd2d5c420")
+
+def get_age_range(min_age, max_age):
+    query = {
+        "$and":[
+            {'age':{'$gte' : min_age}},
+            {'age':{'$lte': max_age}}
+    ]}
+
+    students = studnet_collection.find(query).sort("age")
+
+    for st in students:
+        printer.pprint(st)
+
+# get_age_range(19, 25)
+
+def project_columns():
+    columns = {'_id': 0, 'first_name': 1, "age": 1}
+
+    students = studnet_collection.find({}, columns)
+
+    for st in students:
+        printer.pprint(st)
+
+# project_columns()
