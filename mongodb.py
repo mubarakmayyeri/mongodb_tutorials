13f43ac0264bc2aa2266f3b12a40b793d80f6c02
Mubarak Mayyeri <mubarakmayyeri@gmail.com>
2023-08-07 12:16:44 +0530

    connection to atlas

---
+++ mongodb.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv, find_dotenv
+from pymongo import MongoClient
+import os
+
+load_dotenv(find_dotenv())
+
+
+password = os.environ.get("MONGODB_PWD")
+uri = f"mongodb+srv://mubarakmayyeri:{password}@cluster0.3hujcn7.mongodb.net/?retryWrites=true&w=majority"
+
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
